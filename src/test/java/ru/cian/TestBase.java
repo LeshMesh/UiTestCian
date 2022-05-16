58b6613b8d62f8d3e6b9751f33d52a480942a199
LeshMesh <ivlex37rus@gmail.com>
2022-05-16 13:46:43 +0300

    Added BeforeEach in TestBase

--- src/test/java/ru/cian/TestBase.java
+++ src/test/java/ru/cian/TestBase.java
@@ -1,17 +1,32 @@
-package guru.qa;
+package ru.cian;
 
 import com.codeborne.selenide.Configuration;
-import guru.qa.pages.CianPage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import ru.cian.pages.CianPage;
 import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.*;
 
 public class TestBase {
 
     CianPage cianPage = new CianPage();
 
     @BeforeAll
-    static void setUp() {
+    static void beforeAll() {
         Configuration.baseUrl = "https://cian.ru";
         Configuration.browser = "CHROME";
         Configuration.browserSize = "1920x1080";
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        open("/");
+        sleep(2000); //обход защиты
+    }
+
+    @AfterEach
+    void afterEach() {
+        closeWebDriver();
     }
 }
