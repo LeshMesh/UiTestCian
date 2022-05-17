e5968e511f5d48f6aa98fd5855f8ff1b9f7b5e2b
LeshMesh <ivlex37rus@gmail.com>
2022-05-17 20:04:09 +0300

    Added allure listner

--- src/test/java/ru/cian/TestBase.java
+++ src/test/java/ru/cian/TestBase.java
@@ -1,8 +1,11 @@
 package ru.cian;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import ru.cian.helpers.Attach;
 import ru.cian.pages.CianPage;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -14,6 +17,8 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
         Configuration.baseUrl = "https://cian.ru";
         Configuration.browser = "CHROME";
         Configuration.browserSize = "1920x1080";
@@ -27,6 +32,10 @@ public class TestBase {
 
     @AfterEach
     void afterEach() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
         closeWebDriver();
     }
 }
--- src/test/java/ru/cian/UiTestsCian.java
+++ src/test/java/ru/cian/UiTestsCian.java
@@ -1,70 +1,109 @@
 package ru.cian;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 public class UiTestsCian extends TestBase {
 
     @Test
+    @DisplayName("Авторизация по Email")
     void authorizationToEmailTest() {
-        cianPage.clickLogInHeader()
-                .setValueEmail()
+        step("Открываем форму авторизации", () ->
+            cianPage.clickLogInHeader());
+        step("Вводим Email и пароль", () -> {
+            cianPage.setValueEmail()
                 .setValuePassword()
-                .clickContinueLogIn()
-
-                .checkResultAuthorization();
+                .clickContinueLogIn();
+        });
+        step("Проверяем успешную авторизацию", () ->
+            cianPage.checkResultAuthorization());
     }
 
     @Test
+    @DisplayName("Поиск аренды дома")
     void searchListRentHousePriceCityTest() {
-        cianPage.chooseFilterType()
-                .chooseFilterOffer()
-                .chooseFilterPrice()
-                .chooseFilterGeo()
-                .clickSearch()
+        step("Выбираем фильтр Снять", () ->
+            cianPage.chooseFilterType());
+        step("Выбираем вид недвижимости", () ->
+            cianPage.chooseFilterOffer());
+        step("Устанавливаем стоимость", () ->
+            cianPage.chooseFilterPrice());
+        step("Выбираем город", () ->
+            cianPage.chooseFilterGeo());
+        step("Выполняем поиск", () ->
+            cianPage.clickSearch());
 
-                .checkResultSearch();
+        step("Проверяем результаты поиска", () ->
+            cianPage.checkResultSearch());
     }
 
     @Test
+    @DisplayName("Добавление в избранное в ЛК")
     void addFavoritesAuthorizationTest() {
-        cianPage.choosePopularTab()
-                .saveName()
-                .clickAddFavorite()
-                .checkAddFavoriteTooltip()
-                .clickLogInFavorite()
-                .openPageFavorites()
-                .openFavoriteCard();
-        switchTo().window(1);
-
-        cianPage.checkResultAddFavourite();
+        step("Открываем 1е популярное объявление", () -> {
+            cianPage.choosePopularTab()
+                    .saveName();
+        });
+        step("Добавляем объявление в избранное", () ->
+            cianPage.clickAddFavorite());
+        step("Проверяем в тултипе, что объявление добавилось в избранное", () ->
+            cianPage.checkAddFavoriteTooltip());
+        step("Открываем форму авторизации из тултипа", () ->
+            cianPage.clickLogInFavorite());
+        step("Вводим Email и пароль", () -> {
+            cianPage.setValueEmail()
+                    .setValuePassword()
+                    .clickContinueLogIn();
+        });
+        step("Открываем избранное в личном кабинете", () ->
+            cianPage.openPageFavorites());
+        step("Открываем 1е объявление в избранном", () ->
+            cianPage.openFavoriteCard());
+        step("Проверяем, что это добавленное объявление", () -> {
+            switchTo().window(1);
+            cianPage.checkResultAddFavourite();
+        });
     }
 
     @Test
+    @DisplayName("Добавление в избранное без авторизации")
     void addFavoritesTest() {
-        cianPage.choosePopularTab()
-                .saveName()
-                .clickAddFavorite()
-                .checkAddFavoriteTooltip()
-                .openTooltipHeaderFavourite()
-
-                .checkResultAddFavouriteTooltipHeader();
+        step("Открываем 1е популярное объявление", () -> {
+            cianPage.choosePopularTab()
+                .saveName();
+        });
+        step("Добавляем объявление в избранное", () ->
+            cianPage.clickAddFavorite());
+        step("Проверяем в тултипе, что объявление добавилось в избранное", () ->
+            cianPage.checkAddFavoriteTooltip());
+        step("Открываем избранное в шапке", () ->
+            cianPage.openTooltipHeaderFavourite());
+        step("Проверяем наличие добавленного объявления", () ->
+            cianPage.checkResultAddFavouriteTooltipHeader());
     }
 
     @Test
+    @DisplayName("Изменение города")
     void changeGeoTest() {
-        cianPage.clickGeoSwitcher()
-                .chooseCity()
-
-                .checkChangeGeo();
+        step("Открываем выбор города", () ->
+            cianPage.clickGeoSwitcher());
+        step("Выбираем город", () ->
+            cianPage.chooseCity());
+        step("Проверяем, что выбрался нужный город", () ->
+            cianPage.checkChangeGeo());
     }
 
     @Test
+    @DisplayName("Проверка промо ссылки")
     void promoLinkTest() {
-        cianPage.openPromoCard()
-
-                .checkMainFilter()
-                .checkHeaderPage();
+        step("Открываем ссылку на 1к квартиры", () ->
+            cianPage.openPromoCard());
+        step("Проверяем, что установились верные фильтры", () ->
+            cianPage.checkMainFilter());
+        step("Проверяем заголовок на странице", () ->
+            cianPage.checkHeaderPage());
     }
 }
