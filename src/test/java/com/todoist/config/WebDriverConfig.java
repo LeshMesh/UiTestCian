019085823f9ec42d0c0f5362810f6aa787448569
LeshMesh <ivlex37rus@gmail.com>
2022-05-25 10:07:07 +0300

    Fixed

--- src/test/java/com/todoist/config/WebDriverConfig.java
+++ src/test/java/com/todoist/config/WebDriverConfig.java
@@ -2,12 +2,7 @@ package com.todoist.config;
 
 import org.aeonbits.owner.Config;
 
-@Config.LoadPolicy(Config.LoadType.MERGE)
-@Config.Sources({
-        "system:properties",
-        "classpath:local.properties",
-        "classpath:remote.properties"
-})
+@Config.Sources({"classpath:${typeProperties}.properties"})
 public interface WebDriverConfig extends Config {
 
     @Key("browserName")
